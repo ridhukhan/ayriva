7126ef0c24e5b09715dc34ed2f6ddad2102aedbc
ridhukhan <riyadkhan6598@gmail.com>
2026-08-28 23:09:47 +0600

    update

---
+++ app/secretdashboard/page.js
@@ -0,0 +1,38 @@
+
+"use client"
+import { useRouter } from "next/navigation"
+import {  useEffect, useState } from "react"
+
+export default function SecretDashboard(){
+    const router =useRouter()
+const [isadmin , setISadmin]=useState(null)
+const [loading,setLoading]=useState(true)
+useEffect(()=>{
+async function checkAdmin(){
+    const res =  await fetch("/api/me")
+    const data = await res.json()
+
+    if(data?.customer?.role=== "admin"){
+        setISadmin(true)
+        setLoading(false)
+    }else{
+        router.push("/")
+    }
+}
+
+   checkAdmin()
+},[])
+if(loading){
+<div className="bg-fuchsia-800 font-bold text-3xl text-white">
+    <h1>security checking......</h1>
+</div>
+}
+if(!isadmin)return;
+    return(
+
+        <div>
+
+            <h1>hello Admin ,good evening</h1>
+        </div>
+    )
+}
