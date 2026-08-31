c96b994fcacd4b1803c43b3cf758c1eaba355dbf
Riyad Khan <prakritirshandhane@gmail.com>
2026-08-31 20:25:26 +0600

    Update page.js

--- app/bodycare/page.js
+++ app/bodycare/page.js
@@ -81,7 +81,7 @@ export default function SkincarePage() {
       </Link>
 
       <h1 className="text-3xl font-bold text-center mb-8 text-black mt-6 md:mt-0">
-        Skincare Collection
+        Bodycare Collection
       </h1>
 
       {loading ? (
