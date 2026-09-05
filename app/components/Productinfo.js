c2ec55ef0e89e39f6c74c7de9be817d6748acb67
ridhukhan <riyadkhan6598@gmail.com>
2026-09-05 12:02:08 +0600

    update

--- app/components/Productinfo.js
+++ app/components/Productinfo.js
@@ -5,13 +5,13 @@ export default function ProductInfo(){
 
     return(
 
-        <div className="bg-fuchsia-200 w-fit h-30 flex justify-center text-shadow-pink-800">
+        <div className="bg-fuchsia-200 w-fit h-30 flex justify-center rounded-2xl">
 <div className='flex flex-col'>
-<h1><Truck className='p-3'/><span>Delivery info</span></h1>
+<h1><Truck /><span className='bg-red-600'>Delivery info</span></h1>
 <p>Delivery from today to 2days</p>
 </div>
 <div className='flex flex-col'>
-<h1><TicketPlus className='p-3'/><span>Delivery info</span></h1>
+<h1><TicketPlus className='bg-red-600'/><span>Selling info</span></h1>
 <p>People Are Loving it! sold 100 pcs in last 24 hour</p>
 </div>
 
