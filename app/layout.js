be3ae097bf2b307c92aa176a1259396bf78bf2f8
ridhukhan <riyadkhan6598@gmail.com>
2026-09-20 00:27:59 +0600

    updatee

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
           id="1073286328895666"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `
+            __html:`
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
